355b10f98f516e032c10d8eaeebc72e841508983
Takoo001 <matiasrain73@gmail.com>
2025-05-07 16:27:37 -0400

    Primera clase de python

---
+++ 01-variables.py
@@ -0,0 +1,15 @@
+""" INICIANDO EN PYTHON (VARIABLES)"""
+
+nombre = "Matias"
+
+apellido = "Rain"
+
+# UTILIZANDO VARIABLES EN UN PRINT CON COMAS
+print("Hola, mi nombre es",nombre,"y mi apellido es",apellido)
+
+# IMPRIMIENDO CON OPERADOR DE CONCATENACION
+print("Hola, mi nombre esw " + nombre + "y mi apellido es " + apellido)
+
+# IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
+print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
+
