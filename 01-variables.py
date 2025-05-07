1313040610c421b4679f264105d86b8f2a1fba3d
Takoo001 <matiasrain73@gmail.com>
2025-05-07 16:51:49 -0400

    (Update) Archivo variables

--- 01-variables.py
+++ 01-variables.py
@@ -17,4 +17,4 @@ print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
 
 ciudad, region, pais = "Castro", "Los lagos", "Chile"
 
-print(f"Hola soy de {ciudad},{region},{pais}")
+print(f"Hola soy de {ciudad}, de la region de {region},{pais}")
