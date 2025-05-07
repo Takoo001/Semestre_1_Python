5edd5d78853ef5d3f289564f624ad58a40882209
Takoo001 <matiasrain73@gmail.com>
2025-05-07 16:36:05 -0400

    (Update) Archivo variables

--- 01-variables.py
+++ 01-variables.py
@@ -13,3 +13,8 @@ print("Hola, mi nombre esw " + nombre + "y mi apellido es " + apellido)
 # IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
 print(f"Hola mi nombre es {nombre} y mi apellido es {apellido}")
 
+# INICIALIZANDO MULTI VARIABLES EN UNA SOLA LINEA (NO ES RECOMENDADO)
+
+ciudad, region, pais = "Castro", "Los lagos", "Chile"
+
+print(f"Hola soy de {ciudad},{region},{pais}")
