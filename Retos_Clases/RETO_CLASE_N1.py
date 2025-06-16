d53ee90d88e9b9751a70b0054c5fc1bbdff9cd50
Takoo001 <matiasrain73@gmail.com>
2025-06-16 10:15:35 -0400

    Retos hechos en clases

---
+++ Retos_Clases/RETO_CLASE_N1.py
@@ -0,0 +1,7 @@
+nombre = input("Por favor, Ingresa tu nombre: ")
+edad = int(input("Ahora ingresa tu edad: "))
+
+total_edad = edad + 20
+
+
+print("Hola mi nombre es ",nombre, "tengo ",edad, "años y en 20 años tendre: ",total_edad)
---
+++ Retos_Clases/RETO_CLASE_N2.py
@@ -0,0 +1,7 @@
+inventario = dict(
+    Manzana = (1500, 2000, 1000),
+    Platano = (800, 800, 1700),
+    Cereza = (500, 300, 600)
+)
+
+# RETO NO COMPLETADO
