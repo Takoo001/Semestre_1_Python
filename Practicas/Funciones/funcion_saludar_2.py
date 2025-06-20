b9adf6847b8429ffdd1b465d7ddde86525264de8
Tako <matiasrain73@gmail.com>
2025-06-20 19:35:33 -0400

    Delete Practicas/Funciones directory

--- Practicas/Funciones/funcion_saludar_2.py
+++
@@ -1,5 +0,0 @@
-def saludar(nombre):
-    print("Hola",nombre)
-
-saludar("alex")
-saludar("Python")
--- Practicas/Funciones/funcion_suma.py
+++
@@ -1,4 +0,0 @@
-def suma(a,b):
-    return a + b
-resultado = suma(3, 6)
-print("El resultado es: ",resultado)
